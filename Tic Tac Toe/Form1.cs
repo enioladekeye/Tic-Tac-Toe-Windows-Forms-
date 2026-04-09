075d691cd910823d27483e84a1b5527e1e2d493f
eadekeye <eadekeye@PC-DS118-240195>
2026-04-09 15:24:21 -0500

    Add project files.

---
+++ Tic Tac Toe/Form1.cs
@@ -0,0 +1,51 @@
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+
+        public static string input = "X";
+        bool turn = true;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += when_clicked;
+            button2.Click += when_clicked;
+            button3.Click += when_clicked;
+            button4.Click += when_clicked;
+            button5.Click += when_clicked;
+            button6.Click += when_clicked;
+            button7.Click += when_clicked;
+            button8.Click += when_clicked;
+            button9.Click += when_clicked;
+
+        }
+
+        public void when_clicked(object? sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Text == "")
+            {
+                if (turn)
+                {
+                    button.Text = "X";
+                }
+                else
+                {
+                    button.Text = "O";
+                }
+                turn = !turn;
+            }
+        }
+            
+        public void check_win ()
+        {
+
+        }
+
+        }
+
+    }
